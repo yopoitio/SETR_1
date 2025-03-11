1a9ebefe36beba0f6728ed399f564b8408c38573
yopoitio <rafa.ba@sapo.pt>
2025-03-11 23:42:26 +0000

    First Test

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include "DLL.h"
+#define SIZE 20
 
 int main() {
-    struct Node *dll = MyDLLInit(20);
+    struct Node *dll = MyDLLInit(SIZE);
     MyDLLInsert(dll, 1, "Hello");
+    MyDLLInsert(dll, 3, "!");
+    MyDLLInsert(dll, 2, "World");
+    MyDLLRemove(dll, 2, SIZE);
     return 0;
 }
