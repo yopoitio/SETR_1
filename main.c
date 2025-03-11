3934ad7d6f6d656b6638fd6c8537fea083a66bcc
yopoitio <rafa.ba@sapo.pt>
2025-03-11 23:59:37 +0000

    Add .gitignore and main.c fixes

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 
 int main() {
     struct Node *dll = MyDLLInit(SIZE);
-    MyDLLInsert(dll, 1, "Hello");
-    MyDLLInsert(dll, 3, "!");
-    MyDLLInsert(dll, 2, "World");
-    MyDLLRemove(dll, 2, SIZE);
+    int err = MyDLLInsert(dll, 1, "Hello", SIZE);
+    printf("%d\n", err);
+    err = MyDLLInsert(dll, 3, "!", SIZE);
+    printf("%d\n", err);
+    err = MyDLLInsert(dll, 2, "World", SIZE);
+    printf("%d\n", err);
+    err = MyDLLRemove(dll, 2, SIZE);
+    printf("%d\n", err);
     return 0;
 }
