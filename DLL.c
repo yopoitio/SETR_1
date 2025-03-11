f002c0600a91787c20a929de8e631fd44a236a32
Digicat195 <digicat182@gmail.com>
2025-03-11 23:25:20 +0000

    MyDLLInsert implementation

--- DLL.c
+++ DLL.c
@@ -4,8 +4,26 @@ void MyDLLInit() {
 
 }
 
-void MyDLLInsert() {
+int MyDLLInsert(struct Node *dll, uint16_t newKey, char* data, uint16_t size) {
+    for (int i = 0; i < size; i++) {
+        if (dll[i].key != 0) {
+            dll[i].key = newKey;
+            dll[i].prev = -1;
+            dll[i].next = -1;
+        
+            struct Node *current = dll;
+            while (current->next != NULL)  {
+                if ((current->key > newKey && current->next->key <= newKey) || (current->key == newKey)) {
+                    current->next->prev = i;
+                    current->prev->next = current
+                }
+                current = current->next;
+            }
+        }
+    }
 
+    printf("Array is full\n");
+    return -1;
 }
 
 void MyDLLRemove() {
